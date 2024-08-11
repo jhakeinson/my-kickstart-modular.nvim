71863bcbbc35f2fbc9f03f5ec21e759e4f1a4862
Jhake Inson <me@jhakeinson.com>
2024-08-11 11:52:50 +0800

    Add plugins: nvim-surround and nvim-treesitter-textobjects

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- press jj to exit insert mode
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Exit insert mode' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -30,10 +30,11 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree toggle=true reveal<CR>', desc = 'NeoTree reveal' },
+    { '\\', ':Neotree toggle=true float<CR>', desc = 'NeoTree reveal' },
   },
   opts = {
     filesystem = {
+      hijack_netrw_behavior = 'open_default',
       window = {
         mappings = {
           ['\\'] = 'close_window',
@@ -41,4 +42,24 @@ return {
       },
     },
   },
+  -- require neo-tree on init (no lazy) only when Netrw is opened
+  init = function() -- init is called on startup. i.e. no lazy.
+    vim.g.neo_tree_remove_legacy_commands = 1
+    if vim.fn.argc() >= 1 then
+      vim.api.nvim_create_autocmd('UIEnter', {
+        once = true,
+        callback = function(_)
+          for i = 0, vim.fn.argc() - 1 do -- check for all command line arguments
+            local stat = vim.loop.fs_stat(vim.fn.argv(i))
+            if stat and stat.type == 'directory' then -- only if any of them is a dir
+              require 'neo-tree' -- require neo-tree, which asks lazy to load neo-tree which calls setup with `opts` and
+              -- since hijack_netrw_behavior is set there, neo-tree overwrites netrw on setup
+              return
+            end
+          end
+        end,
+      })
+    end
+    -- if no cl args or all are files, neo-tree is not loaded here and will wait lazily
+  end,
 }
